3bab70a80c79824594a291b91a8b852109089a11
kk5190 <kk5190krrish@gmail.com>
2016-03-26 17:37:53 +0530

    Add Header : Name and Role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+// $("#main").append("Krishna Kumar Singh");
+var name = "Krishna Kumar Singh";
+var role = "Web Developer";
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
